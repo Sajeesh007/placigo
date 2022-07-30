cf2b66412635822268c82a581a6ea0b7a40f6c01
Muhammad Mkm <63127664+MuhammadMkm17@users.noreply.github.com>
2022-07-30 15:22:06 +0530

    supabase spelling change

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,4 +1,4 @@
-import { supabase } from "supabse";
+import { supabase } from "supabase";
 import { createCollege, createStudent } from "./profile.service";
 
 // ------------------------------------------------------ auth ----------------------------------------------------
